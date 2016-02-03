43491c6cb2edfefac03e0ab8d29e6d7cf8d374f7
soltanoff <piccadillable@gmail.com>
2016-02-03 19:46:21 +0300

    Added control of camera view

---
+++ Simple game [SFML]/g_view.cpp
@@ -0,0 +1,61 @@
+#include "g_view.h"
+
+
+
+sf::View view; // определяем объект вида, при помощи которого организуем слежку камеры за игроком
+
+
+
+void set_camera_view(float x, float y) // задаем слежку камеры за объектом находящемся по координатам Х и У
+{
+	if (!(y < 1280 || x < 680))
+		view.setCenter(x + CAMERA_VIEW_X_OFFSET, y);
+}
+
+void view_map(float game_speed)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+	{
+		view.setCenter(view.getCenter().x - game_speed, view.getCenter().y);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+	{
+		view.setCenter(view.getCenter().x + game_speed, view.getCenter().y);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+	{
+		view.setCenter(view.getCenter().x, view.getCenter().y - game_speed);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	{
+		view.setCenter(view.getCenter().x, view.getCenter().y + game_speed);
+	}
+}
+
+void view_control(float game_speed)
+{
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::U))
+	{
+		view.zoom(1.0100f);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::I))
+	{
+		view.zoom(0.999f);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::O))
+	{
+		view.rotate(0.1f); // поворот на 1 градус
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::P))
+	{
+		view.rotate(-0.1f); // поворот на -1 градус
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::K))
+	{
+		view.setSize(1280, 800);
+	}
+	if (sf::Keyboard::isKeyPressed(sf::Keyboard::L))
+	{
+		view.setSize(680, 480);
+	}
+}
---
+++ Simple game [SFML]/include/g_view.h
@@ -0,0 +1,16 @@
+#ifndef G_VIEW
+#define G_VIEW
+#include <SFML\Graphics.hpp>
+
+
+
+extern sf::View view; // определяем объект вида, при помощи которого организуем слежку камеры за игроком
+
+const short CAMERA_VIEW_X_OFFSET = 100;
+
+void set_camera_view(float x, float y); // задаем слежку камеры за объектом находящемся по координатам Х и У
+
+void view_map(float game_speed);
+
+void view_control(float game_speed);
+#endif /* G_VIEW */
