09412e708bf134d126a364f757cdf9ffeb8e99b5
soltanoff <piccadillable@gmail.com>
2016-02-04 21:48:11 +0300

    Updated player sprites size and characteristics

---
+++ Simple game [SFML]/include/textures_settings.h
@@ -0,0 +1,35 @@
+#ifndef TEXTURE_SETTINGS
+#define TEXTURE_SETTINGS
+
+
+
+namespace Player_Texture
+{
+	const char TEXTURE_NAME[] = "hero.png";
+	const short BASE_RECTLEFT_POS = 96;
+
+	// ƒвижение влево MVL
+	const short MVL_LEFT_STEP = 6;
+	const short MVL_TOP = 136;
+	const short MVL_WIDTH = 89;
+	const short MVL_HEIGHT = 55;
+
+	// ƒвижение вправо MVR
+	const short MVR_LEFT_STEP = 6;
+	const short MVR_TOP = 231;
+	const short MVR_WIDTH = 89;
+	const short MVR_HEIGHT = 55;
+
+	// ƒвижение вверх MVU
+	const short MVU_LEFT_STEP = 27;
+	const short MVU_TOP = 305;
+	const short MVU_WIDTH = 39;
+	const short MVU_HEIGHT = 89;
+
+	// ƒвижение вниз MVD
+	const short MVD_LEFT_STEP = 27;
+	const short MVD_TOP = 6;
+	const short MVD_WIDTH = 39;
+	const short MVD_HEIGHT = 89;
+}
+#endif /* TEXTURE_SETTINGS */
