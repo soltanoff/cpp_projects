ed8f52932f6ad44ea6e87bd8a55d325e74f31246
soltanoff <piccadillable@gmail.com>
2016-02-04 21:44:41 +0300

    Added new font for mission textbox

--- Simple game [SFML]/game_fonts.cpp
+++ Simple game [SFML]/game_fonts.cpp
@@ -3,17 +3,27 @@
 
 
 sf::Font main_font;	
+sf::Font mission_font;
 sf::Text score_text; // создаем объект текст. закидываем в объект текст строку, шрифт, размер шрифта(в пикселях);//сам объект текст (не строка)
+sf::Text mission_text;
 
-sf::String score_string = ":3 Score: ";
+sf::String score_string = L"Ваши очки: ";
+sf::String health_string = L"Ваше здоровье: ";
+sf::String game_over_string = L"GAME OVER!\nНажмите ESC.";
+
+void install_text(sf::Text &text, sf::Font &font, sf::String font_name)
+{
+	font.loadFromFile("Font/" + font_name);
+    //score_text.setColor(sf::Color::Red); 
+	text.setPosition(10.0f, 0.0f);
+	text.setFont(font);
+	text.setString("");
+	text.setCharacterSize(30);
+	text.setStyle(sf::Text::Bold); // жирный и подчеркнутый текст. по умолчанию он "худой":)) и не подчеркнутый
+}
 
 void fonts_settings()
 {
-	main_font.loadFromFile("Font/main_font.TTF");
-    //score_text.setColor(sf::Color::Red); 
-	score_text.setPosition(10.0f, 0.0f);
-	score_text.setFont(main_font);
-	score_text.setString("");
-	score_text.setCharacterSize(30);
-	score_text.setStyle(sf::Text::Bold); // жирный и подчеркнутый текст. по умолчанию он "худой":)) и не подчеркнутый
+	install_text(score_text, main_font, "main_font.TTF");//times_new_roman
+	install_text(mission_text, mission_font, "times.TTF");
 }
--- Simple game [SFML]/include/game_fonts.h
+++ Simple game [SFML]/include/game_fonts.h
@@ -1,12 +1,16 @@
-#ifndef GAME_FONTS
+﻿#ifndef GAME_FONTS
 #define GAME_FONTS
 #include <SFML\Graphics.hpp>
 
 
 
-extern sf::Font main_font;	
+extern sf::Font main_font;
+extern sf::Font mission_font;
 extern sf::Text score_text;
+extern sf::Text mission_text;;
 extern sf::String score_string;
+extern sf::String health_string;
+extern sf::String game_over_string;
 
 void fonts_settings();
 #endif /* GAME_FONTS */
