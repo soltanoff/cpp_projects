3fd2f074a9741529d350d024b558c619b292f6ce
soltanoff <piccadillable@gmail.com>
2016-04-05 20:22:21 +0400

    Added Enemy and Bullet classes

--- Simple game [SFML]/include/enemy.h
+++ Simple game [SFML]/include/enemy.h
@@ -4,8 +4,54 @@
 #include "player.h"
 #include "textures_settings.h"
 #include <math.h>
+#include <list>
+
+sf::Image bullet_image;
+
+template<class T>
+class Bullet : public G_Character<T>
+{
+private:
+	void map_iteraction() {}
+public:
+	int k;
+
+	Bullet(T X, T Y, T dX, T dY, sf::Image &image):
+		G_Character(X, Y,
+			Bullet_Texture::RECT_LEFT, 
+			Bullet_Texture::RECT_TOP, 
+			Bullet_Texture::WIDTH, 
+			Bullet_Texture::HEIGHT
+			, image
+		)
+	{
+		this->entity_sprite.setOrigin(this->entity_sprite.getTextureRect().width / 2.0, this->entity_sprite.getTextureRect().height / 2.0);
+		this->dx = dX;
+		this->dy = dY;
+	}
+
+	void search_enemy(Player<T> &enemy)
+	{	
+		T dX = - this->x + enemy.get_x(); // вектор , колинеарный прямой, которая пересекает спрайт и курсор
+		T dY = - this->y + enemy.get_y(); // он же, координата y
+
+		float rotation = (atan2(dY, dX)) * 180 / 3.14159265; // получаем угол в радианах и переводим его в градусы
+		//printf("rotation: %f \n", rotation); // смотрим на градусы в консольке
+		this->entity_sprite.setRotation(rotation); // поворачиваем спрайт на эти градусы	
 
 
+	}
+
+
+
+	void update(float game_speed)
+	{
+		this->x += dx * game_speed;
+		this->y += dy * game_speed;
+	}
+};
+
+const int CRITICAL_DISTANCE = 250;
 
 template<class T>
 class Enemy : public G_Character<T>
@@ -15,6 +61,9 @@ private:
 	sf::String name;
 	void map_iteraction() {}
 public:
+	std::list<Bullet<float>*> enemy_bullets; // создаю список пуль
+	//std::list<Bullet<float>*>::iterator it; // итератор чтобы проходить по эл-там списка
+
 	Enemy(T X, T Y, sf::String enemy_name, sf::Image &image):
 		enemy_spoted(false), name(enemy_name),
 		G_Character(X, Y,
@@ -25,21 +74,41 @@ public:
 			, image
 		)
 	{
+		bullet_image.loadFromFile("Sprites/bullet.png");
+		bullet_image.createMaskFromColor(sf::Color(255, 255, 255));
 		this->entity_sprite.setOrigin(this->entity_sprite.getTextureRect().width / 2.0, this->entity_sprite.getTextureRect().height / 2.0);
 	}
 
 	void search_enemy(Player<T> &enemy)
 	{	
-		T dX = - this->x + enemy.get_x(); // РІРµРєС‚РѕСЂ , РєРѕР»РёРЅРµР°СЂРЅС‹Р№ РїСЂСЏРјРѕР№, РєРѕС‚РѕСЂР°СЏ РїРµСЂРµСЃРµРєР°РµС‚ СЃРїСЂР°Р№С‚ Рё РєСѓСЂСЃРѕСЂ
-		T dY = - this->y + enemy.get_y(); // РѕРЅ Р¶Рµ, РєРѕРѕСЂРґРёРЅР°С‚Р° y
+		T dX = - this->x + enemy.get_x(); // вектор , колинеарный прямой, которая пересекает спрайт и курсор
+		T dY = - this->y + enemy.get_y(); // он же, координата y
 
-		float rotation = (atan2(dY, dX)) * 180 / 3.14159265; // РїРѕР»СѓС‡Р°РµРј СѓРіРѕР» РІ СЂР°РґРёР°РЅР°С… Рё РїРµСЂРµРІРѕРґРёРј РµРіРѕ РІ РіСЂР°РґСѓСЃС‹
-		printf("roptation: %f \n", rotation); // СЃРјРѕС‚СЂРёРј РЅР° РіСЂР°РґСѓСЃС‹ РІ РєРѕРЅСЃРѕР»СЊРєРµ
-		this->entity_sprite.setRotation(rotation); // РїРѕРІРѕСЂР°С‡РёРІР°РµРј СЃРїСЂР°Р№С‚ РЅР° СЌС‚Рё РіСЂР°РґСѓСЃС‹		
+		float rotation = (atan2(dY, dX)) * 180 / 3.14159265; // получаем угол в радианах и переводим его в градусы
+		//printf("rotation: %f \n", rotation); // смотрим на градусы в консольке
+		this->entity_sprite.setRotation(rotation); // поворачиваем спрайт на эти градусы	
+
+		int distance = sqrt( pow(dX, 2) + pow(dY, 2) );
+		printf("distance %i ", distance);
+		if (distance <= CRITICAL_DISTANCE)
+		{
+			if (this->enemy_bullets.empty())
+				this->enemy_bullets.push_back(new Bullet<float>(this->x, this->y, dX, dY, bullet_image));
+			printf("TARGET!");
+		}
+		printf("\n");
 	}
+
+
+	//***************************************************
 	void update(float game_speed)
 	{
+		/*for (it = enemy_bullets; it != enemy_bullets.end(); it++)
+		{
+			(*it)->update(game_speed); //рисуем объекты (сейчас это только враги)
+		}*/
 
+		//(*enemy_bullets.begin())->update(game_speed);
 	}
 };
 #endif /* G_ENEMY */
