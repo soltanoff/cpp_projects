7110d4da2b09bd97b9df6eefee7c15aac120fdde
soltanoff <piccadillable@gmail.com>
2016-04-14 22:53:47 +0400

    Added missions

--- Simple game [SFML]/include/missions.h
+++ Simple game [SFML]/include/missions.h
@@ -7,13 +7,16 @@
 
 extern sf::Texture quest_texture;
 extern sf::Sprite mission_textbox_sprite;
+extern short stone_count;
 //extern short mission_number;
 
 void set_mission_textbox();
 void set_new_mission(short mission_number);
 void get_mission_text(sf::RenderWindow &window, sf::Text &text, int x, int y, short mission_number);
+void get_mission_complete_text(sf::RenderWindow &window, sf::Text &text, int x, int y, short mission_number);
 
 const int MAX_GAME_DIFFICULT = 4;
+const short MAX_MISSIONS_COUNT = 3;
 
 namespace ENEMY_SPAWN
 {// 1280 x 800
