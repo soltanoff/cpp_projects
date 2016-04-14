e585c933ae22e6c4d375198271e463d745595c27
soltanoff <piccadillable@gmail.com>
2016-04-14 22:53:36 +0400

    Added missions

--- Simple game [SFML]/missions.cpp
+++ Simple game [SFML]/missions.cpp
@@ -5,6 +5,7 @@
 sf::Image quest_image;
 sf::Texture quest_texture;
 sf::Sprite mission_textbox_sprite;
+short stone_count;
 //short mission_number;
 
 void set_mission_textbox()
@@ -22,7 +23,11 @@ sf::String get_mission_info(short mission_number)
 	switch(mission_number)
 	{
 	case 1:
-		return L"Вы должны собрать все камни\nдля того, чтобы построить дом.\nБудьте осторожны в лесу много\nрастений как вредных, так и нет.";
+		return L"Уровень №1:\nВы должны собрать 7 камней\nдля того, чтобы построить дом.\nБудьте осторожны в лесу много\nрастений как вредных, так и нет.";
+	case 2:
+		return L"Уровень №2:\nВы должны собрать 10 камней\nдля того, чтобы построить дом.\nБудьте осторожны в лесу много\nрастений как вредных, так и нет.";
+	case 3:
+		return L"Уровень №3:\nВы должны собрать 14 камней\nдля того, чтобы построить дом.\nБудьте осторожны в лесу много\nрастений как вредных, так и нет.";
 	default:
 		return "";
 	}
@@ -34,9 +39,36 @@ void get_mission_text(sf::RenderWindow &window, sf::Text &text, int x, int y, sh
 		y - mission_textbox_sprite.getTextureRect().height / 2.0 + 20);
 	mission_textbox_sprite.setPosition(x - mission_textbox_sprite.getTextureRect().width / 2.0, 
 		y - mission_textbox_sprite.getTextureRect().height / 2.0);
-	
-	text.setString(L"Текущее задание:\n" + get_mission_info(mission_number) + 
+	mission_textbox_sprite.setScale(1.0, 1.0);
+	text.setString(//L"Текущее задание: " + 
+		get_mission_info(mission_number) + 
 		L"\n\nНажмите на левый Shift\nдля отключения подсказки.");
+	window.draw(mission_textbox_sprite);
+	window.draw(text);
+}
+
+void get_mission_complete_text(sf::RenderWindow &window, sf::Text &text, int x, int y, short mission_number)
+{
+	sf::Vector2f pos;
+
+	if (mission_number <= MAX_MISSIONS_COUNT) 
+	{
+		pos.x = x - mission_textbox_sprite.getTextureRect().width / 2.0 - 50;
+		pos.y = y - mission_textbox_sprite.getTextureRect().height / 2.0 + 80;
+		text.setPosition(pos.x + 30, pos.y + 20);
+		mission_textbox_sprite.setPosition(pos);
+		mission_textbox_sprite.setScale(1.2, 0.54);
+		text.setString(L"Поздравляю!\n\nВы собрали нужное количество камней!\nВы переходите на следующий уровень.");
+	}
+	else
+	{
+		pos.x = x - mission_textbox_sprite.getTextureRect().width / 2.0 + 80;
+		pos.y = y - mission_textbox_sprite.getTextureRect().height / 2.0 + 80;
+		text.setPosition(pos.x + 30, pos.y + 20);
+		mission_textbox_sprite.setPosition(pos);
+		mission_textbox_sprite.setScale(0.6, 0.54);
+		text.setString(L"Поздравляю!\n\nВы прошли игру!\n");
+	}
 	window.draw(mission_textbox_sprite);
 	window.draw(text);
 }
@@ -46,19 +78,22 @@ void set_new_mission(short mission_number)
 	switch(mission_number)
 	{
 	case 1: 
+		stone_count = 7;
 		create_random_items(MAP_STONE, 7);
 		create_random_items(MAP_WILDFLOWER, 11);
 		create_random_items(MAP_BUSH, 14);
 		create_random_items(MAP_HEATLHFLOWER, 3);
 		break;
 	case 2:
-		create_random_items(MAP_STONE, 10);
+		stone_count = 10;
+		create_random_items(MAP_STONE, 12);
 		create_random_items(MAP_WILDFLOWER, 14);
 		create_random_items(MAP_BUSH, 14);
 		create_random_items(MAP_HEATLHFLOWER, 3);
 		break;
 	case 3:
-		create_random_items(MAP_STONE, 12);
+		stone_count = 14;
+		create_random_items(MAP_STONE, 14);
 		create_random_items(MAP_WILDFLOWER, 15);
 		create_random_items(MAP_BUSH, 10);
 		create_random_items(MAP_HEATLHFLOWER, 2);
