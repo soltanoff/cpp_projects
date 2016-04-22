327067245ddb9144977bc98659d2ce5e616c55df
soltanoff <piccadillable@gmail.com>
2016-04-22 23:01:34 +0400

    Make Map class

--- Simple game [SFML]/include/simple_map.h
+++ Simple game [SFML]/include/simple_map.h
@@ -1,22 +1,29 @@
 ﻿#ifndef SIMPLE_MAP
 #define SIMPLE_MAP
 #include <SFML\Graphics.hpp>
+#include "main_settings.h"
 
 
+class Map
+{
+private:
+	static sf::String simple_map_structure[MapCFG::MAP_HEIGHT];
 
-const int MAP_HEIGHT = 25; // размер карты высота
-const int MAP_WIDTH = 40; // размер карты ширина 
-const int MAP_TILE_SIZE = 32; // размер тайла спрайта карты
+	sf::Texture map_texture;	// текстура карты
+	sf::Sprite map_sprites;		// создаём спрайт для карты
 
-const char MAP_NOTHING = ' ';
-const char MAP_STONE = 's';
-const char MAP_CURB = '0';
-const char MAP_WILDFLOWER = 'f';
-const char MAP_HEATLHFLOWER = 'h';
-const char MAP_BUSH = 'b';
+public:
+	Map() 
+	{
+		map_texture.loadFromFile("Sprites/map.png");
+		map_sprites.setTexture(map_texture); // заливаем текстуру спрайтом
+	}
 
-extern sf::String simple_map_structure[MAP_HEIGHT];
+	static sf::String* get_map() { return simple_map_structure; }
+	void create_random_items(const char map_item, short count);
+	void draw_map(sf::RenderWindow &window);//, sf::Sprite map_sprites);
 
-void create_random_items(const char map_item, short count);
-void draw_map(sf::RenderWindow &window, sf::Sprite map_sprites);
+	void map_reserve(sf::String *map_copy);
+	void load_map(sf::String *map_copy);
+};
 #endif /* SIMPLE_MAP */
--- Simple game [SFML]/simple_map.cpp
+++ Simple game [SFML]/simple_map.cpp
@@ -2,7 +2,7 @@
 
 
 
-sf::String simple_map_structure[MAP_HEIGHT] = {
+sf::String Map::simple_map_structure[MapCFG::MAP_HEIGHT] = {
 	"0000000000000000000000000000000000000000",
 	"0                                      0",
 	"0                                      0",
@@ -37,18 +37,18 @@ const char MAP_WILDFLOWER = 'f';
 const char MAP_HEATLHFLOWER = 'h';
 const char MAP_BUSH = 'b';
 */
-void create_random_items(const char map_item, short count)
+void Map::create_random_items(const char map_item, short count)
 {
-	srand(time(0));
+	srand((unsigned int)time(0));
 	int rand_x = 0;
 	int rand_y = 0;
 	int temp_count = count;
 
 	while(temp_count != 0)
 	{
-		rand_x = rand() % (MAP_HEIGHT - 1);
-		rand_y = rand() % (MAP_WIDTH - 1);
-		if (simple_map_structure[rand_x][rand_y] == MAP_NOTHING)
+		rand_x = rand() % (MapCFG::MAP_HEIGHT - 1);
+		rand_y = rand() % (MapCFG::MAP_WIDTH - 1);
+		if (simple_map_structure[rand_x][rand_y] == MapCFG::MAP_NOTHING)
 		{
 			simple_map_structure[rand_x][rand_y] = map_item;
 			temp_count--;
@@ -56,36 +56,48 @@ void create_random_items(const char map_item, short count)
 	}
 }
 
-void draw_map(sf::RenderWindow &window, sf::Sprite map_sprites)
+void Map::draw_map(sf::RenderWindow &window)//, sf::Sprite map_sprites)
 {
-	for(int i = 0; i < MAP_HEIGHT; i++)
+	for(int i = 0; i < MapCFG::MAP_HEIGHT; i++)
 	{
-		for(int j = 0; j < MAP_WIDTH; j++)
+		for(int j = 0; j < MapCFG::MAP_WIDTH; j++)
 		{
 			switch(simple_map_structure[i][j])
 			{
-			case MAP_NOTHING: // просто пол
-				map_sprites.setTextureRect(sf::IntRect(0, 0, MAP_TILE_SIZE, MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
+			case MapCFG::MAP_NOTHING: // просто пол
+				map_sprites.setTextureRect(sf::IntRect(0, 0, MapCFG::MAP_TILE_SIZE, MapCFG::MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
 				break;
-			case MAP_STONE: // s - камень
-				map_sprites.setTextureRect(sf::IntRect(MAP_TILE_SIZE, 0, MAP_TILE_SIZE, MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
+			case MapCFG::MAP_STONE: // s - камень
+				map_sprites.setTextureRect(sf::IntRect(MapCFG::MAP_TILE_SIZE, 0, MapCFG::MAP_TILE_SIZE, MapCFG::MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
 				break;
-			case MAP_CURB: // 0 - бордюр
-				map_sprites.setTextureRect(sf::IntRect(2 * MAP_TILE_SIZE, 0, MAP_TILE_SIZE, MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
+			case MapCFG::MAP_CURB: // 0 - бордюр
+				map_sprites.setTextureRect(sf::IntRect(2 * MapCFG::MAP_TILE_SIZE, 0, MapCFG::MAP_TILE_SIZE, MapCFG::MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
 				break;
-			case MAP_WILDFLOWER: // f - дикий цветок
-				map_sprites.setTextureRect(sf::IntRect(3 * MAP_TILE_SIZE, 0, MAP_TILE_SIZE, MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
+			case MapCFG::MAP_WILDFLOWER: // f - дикий цветок
+				map_sprites.setTextureRect(sf::IntRect(3 * MapCFG::MAP_TILE_SIZE, 0, MapCFG::MAP_TILE_SIZE, MapCFG::MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
 				break;
-			case MAP_HEATLHFLOWER: // h - цветок жизни
-				map_sprites.setTextureRect(sf::IntRect(4 * MAP_TILE_SIZE, 0, MAP_TILE_SIZE, MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
+			case MapCFG::MAP_HEATLHFLOWER: // h - цветок жизни
+				map_sprites.setTextureRect(sf::IntRect(4 * MapCFG::MAP_TILE_SIZE, 0, MapCFG::MAP_TILE_SIZE, MapCFG::MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
 				break;
-			case MAP_BUSH: // b - куст
-				map_sprites.setTextureRect(sf::IntRect(5 * MAP_TILE_SIZE, 0, MAP_TILE_SIZE, MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
+			case MapCFG::MAP_BUSH: // b - куст
+				map_sprites.setTextureRect(sf::IntRect(5 * MapCFG::MAP_TILE_SIZE, 0, MapCFG::MAP_TILE_SIZE, MapCFG::MAP_TILE_SIZE)); //если встретили символ пробел, то рисуем 1й квадратик
 				break;
 			}
 
-			map_sprites.setPosition(j * MAP_TILE_SIZE, i * MAP_TILE_SIZE);
+			map_sprites.setPosition((float)(j * MapCFG::MAP_TILE_SIZE), (float)(i * MapCFG::MAP_TILE_SIZE));
 			window.draw(map_sprites);
 		}
 	}
+}
+
+void Map::map_reserve(sf::String *map_copy)
+{
+	for (int i = 0; i < MapCFG::MAP_HEIGHT; i++)
+		map_copy[i] = simple_map_structure[i];
+}
+
+void Map::load_map(sf::String *map_copy)
+{
+	for (int i = 0; i < MapCFG::MAP_HEIGHT; i++)
+		simple_map_structure[i] = map_copy[i];
 }
