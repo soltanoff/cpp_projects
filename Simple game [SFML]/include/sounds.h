45cc3c4724b55f5bc32c59a488f6e265074e3942
soltanoff <piccadillable@gmail.com>
2016-04-22 23:01:03 +0400

    Make Sounds class

--- Simple game [SFML]/include/sounds.h
+++ Simple game [SFML]/include/sounds.h
@@ -3,11 +3,19 @@
 #include <SFML/Audio.hpp>
 
 
+class Sounds
+{
+private:
+	sf::Sound beep_sound; sf::SoundBuffer beep_sound_buffer;
+	sf::Sound fail_sound; sf::SoundBuffer fail_sound_buffer;
+	sf::Sound heal_sound; sf::SoundBuffer heal_sound_buffer;
+	void install_sound(sf::SoundBuffer &sound_buffer, sf::Sound &sound, sf::String file_name);
+	void sounds_settings();
+public:
+	Sounds() { sounds_settings(); }
 
-extern sf::Sound beep_sound;
-extern sf::Sound fail_sound;
-extern sf::Sound heal_sound;
-
-void install_sound(sf::Sound &sound, sf::String file_name);
-void sounds_settings();
+	void play_beep() { beep_sound.play(); }
+	void play_fail() { fail_sound.play(); }
+	void play_heal() { heal_sound.play(); }
+};
 #endif /* G_SOUNDS */
--- Simple game [SFML]/sounds.cpp
+++ Simple game [SFML]/sounds.cpp
@@ -1,21 +1,21 @@
 ﻿#include "sounds.h"
 
 
-
+/*
 sf::Sound beep_sound; sf::SoundBuffer beep_sound_buffer;
 
 sf::Sound fail_sound; sf::SoundBuffer fail_sound_buffer;
 
 sf::Sound heal_sound; sf::SoundBuffer heal_sound_buffer;
+*/
 
-
-void install_sound(sf::SoundBuffer &sound_buffer, sf::Sound &sound, sf::String file_name)
+void Sounds::install_sound(sf::SoundBuffer &sound_buffer, sf::Sound &sound, sf::String file_name)
 {
 	sound_buffer.loadFromFile("Sounds/" + file_name);
 	sound.setBuffer(sound_buffer);
 }
 
-void sounds_settings()
+void Sounds::sounds_settings()
 {
 	install_sound(beep_sound_buffer, beep_sound, "blink.wav");
 	install_sound(fail_sound_buffer, fail_sound, "fail.wav");
