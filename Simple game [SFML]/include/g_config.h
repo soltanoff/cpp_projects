bc3c1e7986027269f9547873269ab307a49debc3
soltanoff <piccadillable@gmail.com>
2016-04-22 23:03:11 +0400

    Added player control into ini file parse

--- Simple game [SFML]/include/g_config.h
+++ Simple game [SFML]/include/g_config.h
@@ -1,7 +1,6 @@
 ﻿#ifndef G_CONFIG
 #define G_CONFIG
 #include <boost\property_tree\ini_parser.hpp>
-const char SETTINGS_FILE[] = "settings.ini";
 
 
 
@@ -19,6 +18,7 @@ private:
 	int _height;
 	int _width;
 	int _time_div;
+	sf::Keyboard::Key _up, _down, _left, _right;
 	/* ============================================================================================================================= */
 	G_Config() {}
 	/* ============================================================================================================================= */
@@ -37,6 +37,10 @@ public:
 	inline int get_width() const { return _width; }
 	inline int get_height() const { return _height; }
 	inline int get_time_div() const { return _time_div; }
+	inline sf::Keyboard::Key get_key_up() { return _up; }
+	inline sf::Keyboard::Key get_key_down() { return _down; }
+	inline sf::Keyboard::Key get_key_left() { return _left; }
+	inline sf::Keyboard::Key get_key_right() { return _right; }
 	/* ============================================================================================================================= */
 	void set_pos(float x, float y) { _pos_x = x; _pos_y = y; }
 	void set_window_size(int w, int h) { _height = h; _width = w; }
@@ -49,7 +53,7 @@ public:
 		_height = pt.get<int>("config.height", 800);
 		
 
-		_fullscreen = pt.get<bool>("config.fullscreen", true);
+		_fullscreen = pt.get<bool>("config.fullscreen", false);
 
 		_pos_x = pt.get<float>("config.pos_x", 80);
 		_pos_y = pt.get<float>("config.pos_y", 80);
@@ -57,6 +61,11 @@ public:
 		_default_mission = pt.get<short>("config.default_mission", 0);	 
 		
 		_time_div = pt.get<int>("config.time_div", 800);
+		
+		_up = (sf::Keyboard::Key)pt.get<int>("config.up", 22);
+		_down = (sf::Keyboard::Key)pt.get<int>("config.down", 18);
+		_left = (sf::Keyboard::Key)pt.get<int>("config.left", 0);
+		_right = (sf::Keyboard::Key)pt.get<int>("config.right", 3);
 	}
 	/* ============================================================================================================================= */
 };
