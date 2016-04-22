c95026def415d0e18d3be46378c36cdcc065c046
soltanoff <piccadillable@gmail.com>
2016-04-22 23:02:35 +0400

    Added main menu and settings menu fields

--- Simple game [SFML]/include/game_fonts.h
+++ Simple game [SFML]/include/game_fonts.h
@@ -1,27 +1,38 @@
 ﻿#ifndef GAME_FONTS
 #define GAME_FONTS
 #include <SFML\Graphics.hpp>
-
+#include "main_settings.h"
 
 
 extern sf::Font main_font;
 extern sf::Font mission_font;
 extern sf::Text score_text;
 extern sf::Text mission_text;;
-extern sf::String score_string;
-extern sf::String health_string;
-extern sf::String game_over_string;
-extern sf::String left_collect;
-
-extern sf::String game_menu[3];
-extern sf::String game_difficult[4];
 
 extern sf::Text mn_new_game;
+extern sf::Text mn_continue;
+extern sf::Text mn_save;
+extern sf::Text mn_load;
+extern sf::Text mn_settings;
 extern sf::Text mn_change_diff;
 extern sf::Text mn_exit;
+extern sf::Text mn_back_to_menu;
 
-const float SCORE_POS[] = { 10, 0 };
+extern sf::Text mn_key_up;
+extern sf::Text mn_key_down;
+extern sf::Text mn_key_left;
+extern sf::Text mn_key_right;
 
-//extern sf::Text mn_difficult;
+extern sf::Text mn_key_up_value;
+extern sf::Text mn_key_down_value;
+extern sf::Text mn_key_left_value;
+extern sf::Text mn_key_right_value;
+
+extern sf::Text mn_accept;
+extern sf::Text mn_cancel;
+
+extern sf::Text mn_setting_caption;
+
 void fonts_settings();
+void install_text(sf::Text &text, sf::Font &font, sf::String font_name);
 #endif /* GAME_FONTS */
