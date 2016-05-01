efdee8f097db5c3c6549047f1cfa8f8f1cd1a524
soltanoff <piccadillable@gmail.com>
2016-05-01 17:56:05 +0400

    Make refactoring of main character class

--- Simple game [SFML]/include/g_character.h
+++ Simple game [SFML]/include/g_character.h
@@ -14,8 +14,8 @@ protected:
 	float speed; // скорость
 	short dir; // направление движения
 	/* ============================================================================================================================= */
-	float x; // позиция по Ох
-	float y; // позиция по ОУ
+	//float x; // позиция по Ох
+	//float y; // позиция по ОУ
 	/* ============================================================================================================================= */
 	sf::String texture_file; // путь к текстуре сущности
 	/* ============================================================================================================================= */
@@ -28,12 +28,12 @@ public:
 	/* ============================================================================================================================= */
 	G_Character(float X, float Y, float textLeft, float textTop, float W, float H, sf::Image &image):
 		//G_Entity(X, Y, textLeft, textTop, W, H, image), 
-		health(FULL_HEALTH), dx(0), dy(0), speed(0), x(X), y(Y)
+		health(FULL_HEALTH), dx(0), dy(0), speed(0)//, x(X), y(Y)
 	{
 		this->entity_texture.loadFromImage(image);
 		
 		this->entity_sprite.setTexture(this->entity_texture);
-		this->entity_sprite.setPosition(x, y);
+		this->entity_sprite.setPosition(X, Y);
 		this->entity_sprite.setTextureRect(sf::IntRect(int(textLeft), int(textTop), int(W), int(H)));
 	}
 	/* ============================================================================================================================= */
@@ -42,8 +42,8 @@ public:
 	inline float get_dx() const { return this->dx; }
 	inline float get_dy() const { return this->dy; }
 	inline float get_speed() const { return this->speed; }
-	inline float get_x() const { return this->x; }
-	inline float get_y() const { return this->y; }
+	inline float get_x() const { return this->entity_sprite.getPosition().x; }
+	inline float get_y() const { return this->entity_sprite.getPosition().y; }
 	inline int get_width() const { return entity_sprite.getTextureRect().width; }
 	inline int get_height() const { return entity_sprite.getTextureRect().height; }
 	inline sf::Sprite get_sprite() const { return this->entity_sprite; }
@@ -53,8 +53,8 @@ public:
 	inline void set_dx(float Acceleration) { this->dx = Acceleration; }
 	inline void set_dy(float Acceleration) { this->dy = Acceleration; }
 	inline void set_speed(float Speed) { this->speed = Speed; }
-	inline void set_x(float X) { this->x = X; }
-	inline void set_y(float Y) { this->y = Y; }
+	inline void set_x(float X) { this->entity_sprite.setPosition(X, get_y()); }
+	inline void set_y(float Y) { this->entity_sprite.setPosition(get_x(), Y); }
 	/* ============================================================================================================================= */
 	inline virtual void health_decr(int value) { this->health -= value; }
 	inline virtual bool is_alive() 
